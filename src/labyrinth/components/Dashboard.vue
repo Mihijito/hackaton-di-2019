a19cffe960e832a44db16d921dfd97e3c099a4f4
Tristan Ortiz-Duchesne <tristan.ortiz-duchesne.1@ulaval.ca>
2019-11-23 10:12:47 -0500

    Fix everything

---
+++ src/labyrinth/components/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dashboard">
+    Whatup
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+};
+</script>
+
+<style scoped>
+@import '../views/Dashboard.scss';
+</style>
---
+++ src/labyrinth/views/Labyrinth.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+      Whatup
+    </div>
+    <Dashboard></Dashboard>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Labyrinth'
+};
+</script>
+
+<style scoped></style>
